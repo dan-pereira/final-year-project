e979a9675cc13438c3b04a7c994b38dd433f5033
byrnj233 <jacob.byrne233@mail.dcu.ie>
2020-02-01 17:26:35 +0000

    server side update

--- src/push.sh
+++ src/push.sh
@@ -1,1 +1,3 @@
+# bulk push whole repo to EC2 instance serving the application
+
 sudo scp -r -i ~/Downloads/fyp2.pem ~/Documents/college/FYP/2020-ca400-byrnj233-pereird2/src/server ubuntu@ec2-18-200-112-1.eu-west-1.compute.amazonaws.com:~/server
--- src/server/flask_app.py
+++ src/server/flask_app.py
@@ -3,10 +3,14 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/<user>')
-def test(user): 
+@app.route('/login/<user>')
+def login(user): 
 	user1 = {'username' : user}
 	return render_template('index.html', title = 'Home', user = user1)
+
+@app.route('/graphs')
+def graph(graph_type):
+	return('graph')
 
 if __name__ == '__main__': 
 	app.run(host = '0.0.0.0', port = 80)
