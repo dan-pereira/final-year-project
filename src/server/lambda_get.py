0799b20f094874f47a1deb28bec5e6b37d8f3c1c
Daniel Pereira <daniel.pereira2@mail.dcu.ie>
2020-05-03 17:02:51 +0100

    update to database_query to allow importing to sensorLearn

--- src/server/lambda_get.py
+++
@@ -1,5 +0,0 @@
-from botocore.vendored import requests
-
-def lambda_handler(event, context):
-   response = requests.get("https://example.com/")
-   print response.json()
--- src/server/src/database_query.py
+++ src/server/src/database_query.py
@@ -1,48 +1,43 @@
-import mysql.connector 
+import mysql.connector
 from mysql.connector import Error
 import os
-from datetime import datetime 
+from datetime import datetime
 
-def query(query): 
-    
-    x = mysql.connector.connect( 
-    user=os.environ['db_username'], 
-    host=os.environ['db_host'], 
-    passwd=os.environ['db_password'], 
+def queryDB(query):
+
+    x = mysql.connector.connect(
+    user=os.environ['db_username'],
+    host=os.environ['db_host'],
+    passwd=os.environ['db_password'],
     database='mydb'
     )
 
     mycursor = x.cursor()
-    mycursor.execute(query) 
+    mycursor.execute(query)
+    result = mycursor.fetchall()
+    #print(result)
+    x.close()
+    return result
 
-    result = mycursor.fetchall() 
+if __name__ == "__main__":
+    result = queryDB('SELECT moisture1,moisture2, moisture3 FROM mydb.sensor_val order by timer desc limit 3')
 
-    x.close() 
-
-    # return result 
+    result = queryDB('SELECT timer, moisture1 FROM mydb.sensor_val order by timer desc limit 5')
 
     value = []
-    for item in result: 
-        # print(type(item))
-        value.append(item) 
-
-    # print(value)
-
+    for item in result:
+        # print(item)
+        value.append(item)
     stamplist = []
     stamplist = [x[0] for x in value]
     moisturelist = [x[1] for x in value]
     stamp = []
     moist  = []
-    for x in stamplist: 
+    for x in stamplist:
         stamp.append(x.strftime('%Y-%m-%d %H:%M:%S'))
 
-    for x in moisturelist: 
+    for x in moisturelist:
         moist.append(x)
 
-    # print(stamp)
-    # print(moisturelist)
+    print(moisturelist)
 
-    return (stamp, moisturelist)
-
-# if __name__ == "__main__":
-#     print(query('SELECT timer, moisture1 FROM mydb.sensor_val order by timer desc limit 5'))
