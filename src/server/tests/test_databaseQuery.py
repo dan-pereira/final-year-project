95af562c9af66fdbec09d3afad81cc313524ab12
Daniel Pereira <daniel.pereira2@mail.dcu.ie>
2020-05-07 09:39:26 +0100

    database query unittests

---
+++ src/server/tests/test_databaseQuery.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+from unittest.mock import Mock
+from unittest.mock import patch
+from src import databaseQuery as dq
+
+
+class TestDatabaseQuery(unittest.TestCase):
+
+    def test_queryDB(self):
+        with patch('mysql.connector.connect') as mock:
+            connection = mock.return_value
+            cursor = connection.cursor.return_value
+            feed = 'test feed'
+            dq.queryDB(feed)
+            mock.assert_called()
+            args = mock.call_args
+            self.assertEqual(4, len(args[1]))
+            cursor.execute.assert_called_with(feed)
+        return
+
+def runDBtest():
+    print('runtTest')
+    unittest.main()
+    print('after')
+    return('finnn')
+
+if __name__ == '__main__':
+    unittest.main()
